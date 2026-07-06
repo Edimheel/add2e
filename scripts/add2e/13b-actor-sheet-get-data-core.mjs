85aba0b0a754999f826a6414ff8079899616462c
Olivier PONS <167159253+Edimheel@users.noreply.github.com>
2026-07-06 13:21:51 +0200

    Share actor effect data preparation with PNJ sheet

--- scripts/add2e/13b-actor-sheet-get-data-core.mjs
+++ scripts/add2e/13b-actor-sheet-get-data-core.mjs
@@ -68,6 +68,26 @@ function add2eLogExceptionalStrengthState(actor, data) {
   });
 }
 
+export function add2ePopulateActorSheetActiveEffectsData(actor, data) {
+  data.activeEffectsList = actor.effects.map(eff => {
+    let desc = eff.getFlag("core", "description") || eff.flags?.add2e?.desc || eff.description || "";
+    if (!desc && eff.flags?.add2e?.tags) desc = "<small>" + eff.flags.add2e.tags.join(", ") + "</small>";
+    let durationStr = "";
+    if (typeof eff.duration?.remaining !== "undefined") durationStr = `${eff.duration.remaining} rounds`;
+    else if (typeof eff.duration?.rounds !== "undefined") durationStr = `${eff.duration.rounds} rounds`;
+    else if (typeof eff.duration?.seconds !== "undefined") durationStr = `${eff.duration.seconds} sec`;
+    return {
+      id: eff.id,
+      name: eff.name || "",
+      img: eff.img || "icons/svg/aura.svg",
+      description: desc,
+      duration: durationStr,
+      sourceName: eff.parent?.name || eff.origin || ""
+    };
+  });
+  return data.activeEffectsList;
+}
+
 globalThis.Add2eActorSheet.prototype.getData = async function getData() {
   const data = this._add2eNativeGetData();
   const state = add2ePrepareActorSheetBaseData({ sheet: this, data });
@@ -87,16 +107,7 @@ globalThis.Add2eActorSheet.prototype.getData = async function getData() {
   });
 
   add2ePopulateActorSheetSpellData({ actor: state.actor, data, items: state.items });
-
-  data.activeEffectsList = this.actor.effects.map(eff => {
-    let desc = eff.getFlag("core", "description") || eff.flags?.add2e?.desc || eff.description || "";
-    if (!desc && eff.flags?.add2e?.tags) desc = "<small>" + eff.flags.add2e.tags.join(", ") + "</small>";
-    let durationStr = "";
-    if (typeof eff.duration?.remaining !== "undefined") durationStr = `${eff.duration.remaining} rounds`;
-    else if (typeof eff.duration?.rounds !== "undefined") durationStr = `${eff.duration.rounds} rounds`;
-    else if (typeof eff.duration?.seconds !== "undefined") durationStr = `${eff.duration.seconds} sec`;
-    return { id: eff.id, name: eff.name || "", img: eff.img || "icons/svg/aura.svg", description: desc, duration: durationStr, sourceName: eff.parent?.name || eff.origin || "" };
-  });
+  add2ePopulateActorSheetActiveEffectsData(this.actor, data);
 
   data.alignementsDisponibles = (state.sys.alignements_autorises && Array.isArray(state.sys.alignements_autorises)) ? state.sys.alignements_autorises : [];
   data.activeTab = this._add2eGetNativeActiveTab?.() || this._add2eActiveTab || this._add2eReadStoredTab?.() || "resume";
