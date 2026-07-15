cb745edfd55cc665c03798d74ed0dbf5979175f6
Olivier PONS <167159253+Edimheel@users.noreply.github.com>
2026-07-15 23:19:57 +0200

    Use guide-defined giant strength profiles

--- scripts/objets_magiques/potion_de_force_de_geant.js
+++ scripts/objets_magiques/potion_de_force_de_geant.js
@@ -19,12 +19,12 @@ if (!isFighter) {
 }
 
 const giantTable = [
-  { giant: "Géant des collines", strength: 19, rockRange: 8, rockDamage: "1d6", doors: "50 %" },
-  { giant: "Géant des pierres", strength: 20, rockRange: 16, rockDamage: "1d12", doors: "60 %" },
-  { giant: "Géant du froid", strength: 21, rockRange: 10, rockDamage: "1d8", doors: "70 %" },
-  { giant: "Géant du feu", strength: 22, rockRange: 12, rockDamage: "1d8", doors: "80 %" },
-  { giant: "Géant des nuages", strength: 23, rockRange: 14, rockDamage: "1d10", doors: "90 %" },
-  { giant: "Géant des tempêtes", strength: 24, rockRange: 16, rockDamage: "1d12", doors: "100 %" }
+  { giant: "Géant des collines", weight: 4500, damage: 7, rockRange: 8, rockDamage: "1d6", doors: "50 %" },
+  { giant: "Géant des pierres", weight: 5000, damage: 8, rockRange: 16, rockDamage: "1d12", doors: "60 %" },
+  { giant: "Géant du froid", weight: 6000, damage: 9, rockRange: 10, rockDamage: "1d8", doors: "70 %" },
+  { giant: "Géant du feu", weight: 7500, damage: 10, rockRange: 12, rockDamage: "1d8", doors: "80 %" },
+  { giant: "Géant des nuages", weight: 9000, damage: 11, rockRange: 14, rockDamage: "1d10", doors: "90 %" },
+  { giant: "Géant des tempêtes", weight: 12000, damage: 12, rockRange: 16, rockDamage: "1d12", doors: "100 %" }
 ];
 
 const typeRoll = await new Roll("1d6").evaluate();
@@ -44,20 +44,25 @@ await durationRoll.toMessage({
 await Add2eEffectsEngine.createTimedCharacteristicEffect({
   actor,
   characteristic: "force",
-  value: giant.strength,
+  value: normalize(giant.giant).replace(/[^a-z0-9]+/g, "_"),
+  displayValue: giant.giant,
+  profile: {
+    degats: giant.damage,
+    poids: giant.weight,
+    ouvrir: giant.doors
+  },
   priority: 100,
   name: "Potion de force de géant",
   img: source?.img,
   sourceItem: source,
   rounds,
   unit: "round",
-  description: `Le consommateur possède la Force d’un ${giant.giant.toLowerCase()} pendant ${durationRoll.total} tours.`,
+  description: `Le consommateur possède une force équivalente à celle d’un ${giant.giant.toLowerCase()} pendant ${durationRoll.total} tours.`,
   tags: [
     "objet_magique",
     "potion",
     "potion:force_de_geant",
-    `force:${giant.strength}`,
-    `geant:${normalize(giant.giant).replace(/[^a-z0-9]+/g, "_")}`,
+    `force_equivalente:${normalize(giant.giant).replace(/[^a-z0-9]+/g, "_")}`,
     "lancer_rochers",
     `rocher_portee:${giant.rockRange}`,
     `rocher_degats:${giant.rockDamage}`,
@@ -68,6 +73,8 @@ await Add2eEffectsEngine.createTimedCharacteristicEffect({
     potion: true,
     potionSlug: "force_de_geant",
     giantType: giant.giant,
+    giantDamageAdjustment: giant.damage,
+    giantWeightAllowance: giant.weight,
     rockRange: giant.rockRange,
     rockDamage: giant.rockDamage,
     doors: giant.doors
@@ -75,11 +82,12 @@ await Add2eEffectsEngine.createTimedCharacteristicEffect({
 });
 
 await Add2eEffectsEngine.postGenericEffectChat(actor, "Potion de force de géant", `
-  <p>Type obtenu : <b>${giant.giant}</b>.</p>
-  <p>Force temporaire : <b>${giant.strength}</b>.</p>
+  <p>Force équivalente : <b>${giant.giant}</b>.</p>
+  <p>Ajustement aux dégâts : <b>+${giant.damage}</b>.</p>
+  <p>Poids permis : <b>+${giant.weight}</b>.</p>
   <p>Durée : <b>${durationRoll.total} tours</b> (${rounds} rounds).</p>
   <p>Lancer de rochers : portée <b>${giant.rockRange}\"</b>, dégâts <b>${giant.rockDamage}</b>.</p>
-  <p>Ouverture des portes : <b>${giant.doors}</b>.</p>
+  <p>Portes : <b>${giant.doors}</b>.</p>
 `, source);
 
 return true;
