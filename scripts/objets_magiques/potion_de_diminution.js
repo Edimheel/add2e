f472938045dc3c758a8c0a5f88e181c990862eba
Olivier PONS <167159253+Edimheel@users.noreply.github.com>
2026-07-30 23:09:11 +0200

    Canonicalize diminution transformation size

--- scripts/objets_magiques/potion_de_diminution.js
+++ scripts/objets_magiques/potion_de_diminution.js
@@ -5,6 +5,16 @@ return Add2eEffectsEngine.applyConfiguredEffect({ actor, item, sourceItem, sort,
   selfOnly: true,
   durationFormula: "1d4+7",
   durationMultiplier: 10,
-  tags: ["transformation", "taille:5_pourcent", "mouvement:adapte"],
+  tags: ["transformation", "forme:diminution", "taille:5_pourcent"],
+  extraFlags: {
+    capabilityTransformation: {
+      kind: "form",
+      sourceKey: "potion:diminution",
+      formKey: "diminution",
+      label: "Potion de diminution",
+      size: "5_pourcent",
+      sizeFactor: 0.05
+    }
+  },
   rule: "La potion entière réduit le consommateur et son équipement à 5 % de leur taille normale. La durée est de 6 tours plus 2 à 5 tours."
 });
