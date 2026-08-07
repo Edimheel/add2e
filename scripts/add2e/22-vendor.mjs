6d4d192ca4453af9dc02b6defc236ddca316f924
Olivier PONS <167159253+Edimheel@users.noreply.github.com>
2026-08-07 16:16:05 +0200

    Remove projectile attack wrapper installation

--- scripts/add2e/22-vendor.mjs
+++ scripts/add2e/22-vendor.mjs
@@ -5,7 +5,6 @@ import {
   ADD2E_VENDOR_VERSION,
   VENDOR_SETTING,
   registerRecoveryHooks,
-  patchAttackRollProjectileConsumption,
   patchActorSheetMoney,
   registerGlobals,
   isVendorActor,
@@ -538,7 +537,6 @@ Hooks.once("ready", async () => {
   await enforceShopTokenPresentation().catch(err => console.warn("[ADD2E][SHOP][TOKEN_PRESENTATION]", err));
 
   registerRecoveryHooks();
-  patchAttackRollProjectileConsumption();
   patchActorSheetMoney();
   patchVendorTokenClick();
   patchArmorerTokenClick();
